071105f5b445b2a28dcce80ccb6e068cd869ff21
anavat123 <45746534+anavat123@users.noreply.github.com>
2018-12-10 15:52:24 -0500

    Update Auto.ino

--- Auto.ino
+++ Auto.ino
@@ -31,6 +31,7 @@ state = DRIVING_FORWARD;
 pinMode(limitSwitchPort,INPUT);
 pinMode(A3,INPUT); //right line sensor
 pinMode(A4,INPUT); //left line sensor
+pindMode(A5,INPUT); //middle line sensor
 lcd.begin(16,2);
 lcd.print("Ready!");
 delay(1000)
@@ -44,9 +45,26 @@ void DoDriving() {
   switch(state) {
     case DRIVING_FORWARD:
       lcd.
-     if((analogRead(A3)>t) && (analogRead(A4)>t))
-       leftMotor.write()
-       rightMotor.write() //go straight
+     if((analogRead(A3)>t) && (analogRead(A4)>t) && (analogRead(A5)<t))  //both sensor sees the carpet, and middle sensor sees line so drive forward
+       leftMotor.write();
+       rightMotor.write(); //go straight, add values depending on test
+     if((analogRead(A3)<t) && (analogread(A4)>t)) && (analogRead(A5)>t)) // right line sensor sees the line so turn left, right motor> left motor
+       leftMotor.write();
+       rightMotor.write();
+     if((analogRead(A3)>t) && (analogread(A4)<t)) && (analogRead(A5)>t)) // left line sensor sees the line so turn right, right motor>left motor
+       leftMotor.write();
+       rightMotor.write();
+     if((analogRead(A3)<t) && (analogread(A4)>t)) && (analogRead(A5)<t))       // left and middle sensor sees white line, switch state to turning
+       state = TURNING;
+    case TURNING:
+      lcd.setCursor(0,0);
+      lcd.clear();
+      lcd.print("TURNING");
+      delay(
+      leftMotor.write()
+      rightMotor.write()
+      delay()
+       
       // raise lift and drive at same time
       
 }
