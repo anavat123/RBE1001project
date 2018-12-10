3327143968d784379b98d180758aeaed5b014af5
anavat123 <45746534+anavat123@users.noreply.github.com>
2018-12-09 22:22:52 -0500

    Update Final_RC_Code.ino

--- Final_RC_Code.ino
+++ Final_RC_Code.ino
@@ -8,9 +8,14 @@ Servo leftmotor;
 Servo liftMotor;
 Servo intakeMotor;
 
+
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600)
+leftmotor.attach(,1000,2000)
+rightmotor.attach(,1000,2000)
+intakeMotor.attach(,1000,2000)
+liftMotor.attach(,1000,2000)
 dfw.begin();
 dfw.update();
 while (dfw.start() == 0) {
@@ -26,12 +31,21 @@ void loop() {
  leftmotor.write(dfw.joysticklv());
  rightmotor.write(180 - dfw.joystickrv());
  if (dfw.l2() == 0) {
- intakeMotor.write(180) //need to determine direction of motor
+ intakeMotor.write(180); //need to determine direction of motor
  }
  if (dfw.l1() == 0) {
-  intakeMotor.write(0)
+  intakeMotor.write(0);
  }
+ if (dfw.one() == 0) {
+  liftMotor.write(0);
+    dfw.update();
+ }
+  if (dfw.three() == 0) {
+  liftMotor.write(180);
+    dfw.update();
+ }
+ 
  
  
 }
-if 
+ 
